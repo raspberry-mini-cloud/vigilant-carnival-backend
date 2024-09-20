4863db9cd46075e087a466dfd62567fcead8d63d
Adam Andersson <adam.andersson@afry.com>
2024-09-20 08:42:00 +0200

    Add methods for populating the cache and getting all data within it

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -31,11 +31,53 @@ func InitRedis(ctx context.Context) {
     fmt.Println(pong)
 }
 
+func PopulateRedisCache(temperatures []TemperatureData) {
+	for _, temperature := range temperatures {
+		AddTemperatureData(temperature.Timestamp.Unix(), temperature.Temperature)
+	}
+}
+
 func AddTemperatureData(timestamp int64, temperature float64) {
 	err := RedisClient.Do(Context, "TS.ADD", "temperature_readings", timestamp, temperature).Err()
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func GetAllTemperatureData()([]TemperatureData) {
+	result, err := RedisClient.Do(Context, "TS.RANGE", "temperature_readings", 0, time.Now().Unix()).Result()
+	if err != nil {
+		log.Fatalln("Could not read values from redis")
+	}
+
+	// Type assertion to ensure result is the expected type (e.g., slice of interface{})
+	entries, ok := result.([]interface{})
+	if !ok {
+		// Handle unexpected type
+		fmt.Println("Unexpected result type")
+		return nil
+	}
+
+	var temperatureData []TemperatureData
+
+	// Process each entry
+	for _, entry := range entries {
+		// Assuming each entry is also a slice (timestamp, value)
+		data, ok := entry.([]interface{})
+		if !ok || len(data) != 2 {
+			fmt.Println("Unexpected entry format")
+			continue
+		}
+
+		// Extract timestamp and value
+		timestamp := data[0].(int64) // Convert to int64 if necessary
+		temp := data[1].(float64)   // Convert to float64 or your expected data type
+
+		temperatureData = append(temperatureData, TemperatureData{ Timestamp: time.Unix(timestamp/1000, 0), Temperature: temp})
+
+	}
+	return temperatureData
+
 }
 
 func CheckKeyExists(key string)(bool) {
